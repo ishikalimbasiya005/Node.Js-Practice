0c9146fc1dce5078839c7c87b290b7753089de67
Ishika Limbasiya <ishikalimbasiya005@gmail.com>
2025-07-29 11:47:23 +0800

    Initial commit for Node.js practice files

---
+++ Import-Export/export.js
@@ -0,0 +1,16 @@
+module.exports = {
+
+    // Example-1:---------------
+    RollNo : 1,
+    Name : "Radha",
+    Gujrati : 90,
+    English : 85,
+    Hindi : 67,
+    Total : 250,
+    Average : 82,
+    Class : "Distinction" 
+    
+    // Example-2:---------------------
+    //  arr : [1,2,3,4,5,6,7,8,9,10]
+}
+
---
+++ Import-Export/import.js
@@ -0,0 +1,14 @@
+const Mycode = require("./export")
+
+// Example-1:--------
+console.log(Mycode.RollNo);
+console.log(Mycode.Name);
+console.log(Mycode.Gujrati);
+console.log(Mycode.English);
+console.log(Mycode.Hindi);
+console.log(Mycode.Total);
+console.log(Mycode.Average);
+console.log(Mycode.Class);
+
+// Example-2:-------------------
+// console.log(Mycode.arr);
