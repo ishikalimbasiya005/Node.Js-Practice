a114255f4da8e007007b0ef2fee899eda46b096a
Ishika Limbasiya <ishikalimbasiya005@gmail.com>
2025-07-29 12:04:21 +0800

    Initial commit for Node.js practice files

---
+++ Practice/practice.js
@@ -0,0 +1,24 @@
+    // +++++++++++++++++++++++ Install Command +++++++++++++++++++++++++++++
+// Install Package.json file :- npm init -f
+// Install Package-lock.json file :- npm i
+// Install Colors in Output :- npm i colors
+// Install Automatic run Prgram :- npm i nodemon -g
+
+
+// Add Color :----------------------
+const color = require ('colors');
+
+console.log("Hello World!".bgRed);
+
+// Show Directory name :-------
+console.log(__dirname);
+
+// Show File name :---------
+console.log(__filename);
+
+
+
+
+
+
+
